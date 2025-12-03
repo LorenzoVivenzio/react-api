f86e4a77a9eb0c864cd6d67d9d54f02b433e5fd3
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-12-03 17:38:37 +0100

    feat: card aggiunte

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import axios from "axios"
 import { useEffect, useState } from 'react'
 import './App.css'
-import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css"
+
 
 function App() {
-  const [dati, setDati] = useState([])
+  const [attori, setAttori] = useState([])
 
-  axios.get("https://lanciweb.github.io/demo/api/actresses/")
-    .then((resp) => {
-      setDati(resp.data.response)
-    })
+  useEffect(() => {
+    fecthAttori()
+  }, [])
+
+
+  function fecthAttori() {
+    axios
+      .get("https://lanciweb.github.io/demo/api/actors/")
+      .then((resp) => {
+        setAttori(resp.data)
+      })
+  }
+
 
   return (
     <>
-      
+    <div className="container">
+      <h1 className="text-center mt-3">Attori</h1>
+      <div className="row">
+        {attori.map((attore) =>(
+        <div className="col-4">
+          <div className="card">
+            <h3 className="text-center">{attore.name}</h3>
+            <img src={attore.image} alt="" />
+            <p className="p-3">biography: {attore.biography}</p>
+          </div>
+        </div>
+        ))}
+
+      </div>
+    </div>
     </>
   )
 }
